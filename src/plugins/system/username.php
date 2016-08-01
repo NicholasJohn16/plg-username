c2487cd4aafc249d1a0e47697fb7bcc9855ba7a5
Nick Swinford <nicholasjohn16@gmail.com>
2016-08-01 04:03:01 -0400

    fix incorrect identifier

--- src/plugins/system/username.php
+++ src/plugins/system/username.php
@@ -7,6 +7,6 @@ class plgSystemUsername extends JPlugin
 	public function onAfterRoute() { 
 		//map the actor helper class to a custom class PlgSystemActorhelper 
 		KService::setAlias('com://site/actors.template.helper','plg://site/system.username.helper'); 
-		KService::setAlias('com://site/actors.template.helper.story','plg://site.system.username.story');
+		KService::setAlias('com://site/actors.template.helper.story','plg://site/system.username.story');
 	} 
 } 
