a3f580a3f5e1bce8801411ea80512b9a83e1956c
Nick Swinford <nicholasjohn16@gmail.com>
2018-05-14 19:50:46 -0400

    change event to onBeforeDispatch

--- src/plugins/system/username.php
+++ src/plugins/system/username.php
@@ -2,7 +2,7 @@
 
 class plgSystemUsername extends PlgAnahitaDefault 
 {
-	public function onAfterRoute() { 
+	public function onBeforeDispatch() { 
 		//map the actor helper class to a custom class PlgSystemActorhelper 
 		KService::setAlias('com:actors.template.helper','plg://site/system.username.helper'); 
 		KService::setAlias('com://site/actors.template.helper.story','plg://site/system.username.story');
